9ad781da6d76313631bb0ff239f5a081fd9bf49c
rvenegasvg <112522002+rvenegasvg@users.noreply.github.com>
2023-01-18 22:48:13 -0400

    Update leerinput.cs

--- DevVideojuegos/Assets/Scripts/leerinput.cs
+++ DevVideojuegos/Assets/Scripts/leerinput.cs
@@ -14,9 +14,18 @@ public class leerinput : MonoBehaviour
         cadena = inputField.text;
         Debug.Log(cadena);
 
-        PlayerPrefs.SetString("nombre",cadena);
+        if (string.IsNullOrEmpty(cadena))
+        {
+            PlayerPrefs.SetString("nombre", cadena);
+            Debug.Log("el nombre en el playerprefs es: " + PlayerPrefs.GetString("nombre"));
+        }
+        else
+        {
+            Debug.Log("no guardo ningun dato");
+        }
+       
 
-        Debug.Log("el nombre en el playerprefs es: "+PlayerPrefs.GetString("nombre"));
+       
     }
 
 
